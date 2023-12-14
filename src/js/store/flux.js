5562a549c51cb3b0e7de0d12ef8e463ed153e83c
andyymor <61852990+andyymor@users.noreply.github.com>
2023-12-14 20:03:20 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,31 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
+			contactList: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
+				  "fullName": "John Doe",
+				  "address": "123 Main Street, Cityville",
+				  "phone": "555-1234",
+				  "email": "john.doe@example.com"
 				},
 				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
+				  "fullName": "Jane Smith",
+				  "address": "456 Oak Avenue, Townsville",
+				  "phone": "555-5678",
+				  "email": "jane.smith@example.com"
+				},
+				{
+				  "fullName": "Bob Johnson",
+				  "address": "789 Pine Road, Villagetown",
+				  "phone": "555-9101",
+				  "email": "bob.johnson@example.com"
 				}
-			]
+			  ]
+			  
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			
+		
 		}
 	};
 };
