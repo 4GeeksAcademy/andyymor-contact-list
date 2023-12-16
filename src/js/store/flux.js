e68a4f8d0ea2bb4e2f89e3c5252aea8139a68dea
andyymor <61852990+andyymor@users.noreply.github.com>
2023-12-16 01:58:09 +0000

    third commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,28 +3,45 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			contactList: [
 				{
-				  "fullName": "John Doe",
-				  "address": "123 Main Street, Cityville",
-				  "phone": "555-1234",
-				  "email": "john.doe@example.com"
+					"fullName": "John Doe",
+					"address": "123 Main Street, Cityville",
+					"phone": "555-1234",
+					"email": "john.doe@example.com"
 				},
 				{
-				  "fullName": "Jane Smith",
-				  "address": "456 Oak Avenue, Townsville",
-				  "phone": "555-5678",
-				  "email": "jane.smith@example.com"
+					"fullName": "Jane Smith",
+					"address": "456 Oak Avenue, Townsville",
+					"phone": "555-5678",
+					"email": "jane.smith@example.com"
 				},
 				{
-				  "fullName": "Bob Johnson",
-				  "address": "789 Pine Road, Villagetown",
-				  "phone": "555-9101",
-				  "email": "bob.johnson@example.com"
+					"fullName": "Bob Johnson",
+					"address": "789 Pine Road, Villagetown",
+					"phone": "555-9101",
+					"email": "bob.johnson@example.com"
 				}
-			  ]
-			  
+			]
+
 		},
 		actions: {
-			
+			addContact:async(fullName, email, phone, address) => {
+				const respone = await fetch("",{
+					method:"POST",
+					body: JSON.stringify({
+						full_name: fullName,
+						phone: phone,
+						email: email,
+						address: address,
+						agenda_slug: "agenda",
+						
+					}),
+					headers: {"Context-Type": "application/JSON"}
+				})
+				const data = await respone.json()
+				setStore({contactList:[...getStore().contactList,data]})
+			}, 
+			updateContact: (contactId, fullName, email, phone, address) => {},
+			deleteContact: (contactId) => {}
 		
 		}
 	};
